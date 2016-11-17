bebcefde5bd2d725156575037cb147d618a336f5
mishab <bielanm.box@gmail.com>
2016-11-17 18:40:01 +0200

    redirect to dashboard from login

---
+++ client/app/app.const.js
@@ -0,0 +1,4 @@
+'use strict'
+
+angular.module('kpilance')
+	.constant('API', 'http://127.0.0.1:8080');
--- client/app/auth.service.js
+++ client/app/auth.service.js
@@ -1,19 +1,57 @@
 'use strict';
 
 angular.module('kpilance.auth')
-	.service('AuthService', function() {
-		
-		this.login = function(user) {
+	.service('AuthService', function($http, $localStorage, JWTService, API) {
+
+		this.login = (user) => {
+			// return $http({
+			// 	url: API + '/auth',
+			// 	method: 'POST',
+			// 	data: user
+
+			// }).then((response) => {
+			// 	$localStorage.currentUser = response.user;
+			// 	JWTService.save(response.token);
+
+			// 	$http.defaults.headers.common.Authorization = JWTService.getToken();
+			// 	return response;
+			// });
+
 			return new Promise((resolve, reject) => {
-				if(user.username == user.password)
+				if(user.username == user.password) {
+					$localStorage.currentUser = user;
 					resolve();
-				else
+				}
+				else {
 					reject();
+				}
 			});
 		};
 
-		this.logout = function() {
-			console.log('logout atempt');
+		this.logout = () => {
+			$localStorage.currentUser = null;
+
+			// JWTService.destroy();
+			// $http.defaults.headers.common.Authorization = '';
+
 		};
 
+		this.authAtemp = () => {
+			return $localStorage.currentUser;
+		}
+
+	})
+	.service('JWTService', function($localStorage) {
+
+		this.save = (token) => {
+			$localStorage.token = token;
+		};
+
+		this.destroy = () => {
+			$localStorage.token = null;
+		};
+
+		this.getToken = () => {
+			return $localStorage.token;
+		}
 	});
